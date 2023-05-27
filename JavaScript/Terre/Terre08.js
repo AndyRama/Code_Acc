067f6da6e9d91c2a858c59c06f3b602c18440fad
Andy Ramaroson <andyramaroson@gmail.com>
2023-05-27 10:24:17 +0200

    [fix] Terre07-08 clean code and context

--- JavaScript/Terre/Terre08.js
+++ JavaScript/Terre/Terre08.js
@@ -1,4 +1,22 @@
+// Créez un programme qui affiche le résultat d’une puissance.
+
+// L’exposant ne pourra pas être négatif.
+
+// Exemples d’utilisation :
+// $> node exo.js 2 3
+// 8
+
+// Attention : je compte sur vous pour gérer les potentielles erreurs d’arguments.
+
+///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
+
+// Récupérer les arguments
 // Le programme prendra deux arguments : un nombre et un exposant.
+// Les arguments doivent être des nombres.
+// L’exposant ne pourra pas être négatif.
+// Affiche le resultat nombre  à la puissance de l'exposant.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const nombre = process.argv[2];
 const exposant = process.argv[3];
@@ -7,7 +25,7 @@ if (process.argv.length !== 4) {
     console.log("Missing arguments");
     process.exit(1);
 
-// Les arguments doivent être des nombre.
+// Les arguments doivent être des nombres.
 } else if (isNaN(nombre) || isNaN(exposant)) {
     console.log("One of the arguments is not a number");
     process.exit(1);
