0903dfb8ad8105c89481e6e1dfb2ae3e67285a1c
Andy Ramaroson <andyramaroson@gmail.com>
2023-05-29 17:32:22 +0200

    (feat) clean code Terre and begin Eau

--- Terre/terre15.js
+++ Terre/terre15.js
@@ -14,7 +14,7 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function victoire() {
-  console.log("J’ai terminé l’Épreuve de la Terre et c’était facile.")
+  console.log("J'ai terminé l'Épreuve de la Terre et c'était facile.")
 }
 
 victoire()
