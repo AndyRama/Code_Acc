56e2cb86b8553780bd638e8078cdccc927d7f2ea
Andy Ramaroson <andyramaroson@gmail.com>
2023-05-31 21:37:20 +0200

    (feat) afficher les arguments reçus à l’envers.

--- Eau/eau02.js
+++ Eau/eau02.js
@@ -1,25 +1,21 @@
-// Créez un programme qui affiche ses arguments reçus à l’envers.
-
-// Exemples d’utilisation :
-// $> python exo.py “Suis” “Je” “Drôle”
-// Drôle
-// Je
-// Suis
-
-// $> python exo.py ha ho
-// ho
-// ha
-
-// $> python exo.py “Bonjour 36”
-// Bonjour 36
-
-// Afficher error et quitter le programme en cas de problèmes d’arguments.
-
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
-// récupérer les arguments
-// Enlever le premier argument qui est le nom du fichier
-// Inverser les arguments
-// Afficher les arguments
+// Récupérer les arguments passés en ligne de commande
+// Inverser l'ordre des éléments dans le tableau
+// Afficher les arguments et joindre en une seule chaîne de caractères séparée par des sauts de ligne
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Récupérer les arguments passés en ligne de commande
+const args = process.argv.slice(2);
+
+// version final (refacto)
+let i = 0;
+let result = '';
+
+while (i < args.length) {
+  // Inverser l'ordre des éléments dans le tableau
+  // Afficher les arguments et joindre en une seule chaîne
+  result = result + args.reverse().join('\n');
+  i++;
+}
