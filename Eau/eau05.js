145dde32a0d2b33e03ff8c3890ecfcfb053b57c8
Andy Ramaroson <andyramaroson@gmail.com>
2023-06-10 19:21:18 +0200

    (feat) pseudo code exo : 5

--- Eau/eau05.js
+++ Eau/eau05.js
@@ -14,6 +14,35 @@
 
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
-
+//Déterminer l'argument
+//Si l'argument est présent || un nombre
+    //Tableau 1 ["bonjour" => "b", "o", "n" ...]
+    //Tableau 2 ["jour" => "j", "o", "u", "r" ...]  //Include
+      //Si meme return true
+      //sinon return False
+//sinon afficher error
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Déterminer l'argument
+const args = process.argv.slice(2)
+
+//Si l'argument est présent || un nombre
+if (args.length === 2 && !isNaN(args[0]) && !isNaN(args[1])) {
+  console.log(args.includes(`${args[1]}`));//CONDITION 
+  //Si meme return true
+  //sinon return False
+}
+
+
+
+
+
+
+  //Tableau 2 ["jour" => "j", "o", "u", "r" ...] 
+  // console.log(args.includes(`${args[1]}`));//CONDITION 
+  //Si meme return true
+  //sinon return False
+//sinon afficher error
+
+console.log(args);
