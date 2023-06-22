409109652e58e0b3ac451e0941868cbbda17aacb
Andy Ramaroson <andyramaroson@gmail.com>
2023-06-22 23:22:58 +0200

    (feat) create a firstLetterCapitalize

--- Eau/eau07.js
+++ Eau/eau07.js
@@ -1,16 +1,56 @@
-// Créez un programme qui met en majuscule la première lettre de chaque mot d’une chaîne de caractères. Les autres lettres devront être en minuscules. Les mots ne sont délimités que par un espace, une tabulation ou un retour à la ligne.
+// Créez un programme qui met en majuscule la première lettre de chaque mot d’une chaîne de caractères. Les autres lettres devront être en minuscules. 
+// Les mots ne sont délimités que par un espace, une tabulation ou un retour à la ligne.
 
 // Exemples d’utilisation :
-// $> python exo.py “bonjour mathilde, comment vas-tu ?!”
+// $> node exo.js “bonjour mathilde, comment vas-tu ?!”
 // Bonjour Mathilde, Comment Vas-tu ?!
 
-// $> python exo.py 42
+// $> node exo.js 42
 // error
 
 // Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
-
+// Vérification du nombre d'arguments
+// Récupération de la chaîne de caractères depuis le premier argument
+// Vérification si la chaîne est vide
+// Créer une fonction pour mettre en majuscule la première lettre d'un mot
+// Séparation de la chaîne de caractères en mots
+// Transformation de chaque mot en mettant en majuscule la première lettre
+// Reconstruction de la chaîne de caractères avec les mots transformés
+// Affichage du résultat
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Vérification du nombre d'arguments
+if (process.argv.length !== 3) {
+  console.error("Error");
+  process.exit(1);
+}
+
+// Récupération de la chaîne de caractères depuis le premier argument
+const inputString = process.argv[2];
+
+// Vérification si la chaîne est vide
+if (inputString.length === 0) {
+  console.error("Error");
+  process.exit(1);
+}
+
+// Fonction pour mettre en majuscule la première lettre d'un mot
+function capitalizeFirstLetter(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
+
+// Séparation de la chaîne de caractères en mots
+const words = inputString.split(/\s+/);
+
+// Transformation de chaque mot en mettant en majuscule la première lettre
+const capitalizedWords = words.map(capitalizeFirstLetter);
+
+// Reconstruction de la chaîne de caractères avec les mots transformés
+const outputString = capitalizedWords.join(" ");
+
+// Affichage du résultat
+console.log(outputString);
