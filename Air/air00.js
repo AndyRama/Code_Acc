6fa5f6d458005849099a6ee4a76c4a3178e8cfa7
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-06 23:52:25 +0200

    [enhancement] add context training

--- Air/air00.js
+++ Air/air00.js
@@ -1,4 +1,5 @@
-// Créez un programme qui découpe une chaîne de caractères en tableau (séparateurs : espaces, tabulations, retours à la ligne).
+// Créez un programme qui découpe une chaîne de caractères en tableau 
+// (séparateurs : espaces, tabulations, retours à la ligne).
 
 // Votre programme devra utiliser une fonction prototypée comme ceci :
 // ma_fonction(string_à_couper, string_séparateur) { // syntaxe selon votre langage
