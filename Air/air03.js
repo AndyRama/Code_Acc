8721106be52ba7cf61386d1c931bab83858d4295
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-10 22:52:34 +0200

    [Enhancemenet] exo air pseudo code invalide

--- Air/air03.js
+++ Air/air03.js
@@ -2,10 +2,10 @@
 
 
 // Exemples d’utilisation :
-// $> python exo.py 1 2 3 4 5 4 3 2 1
+// $> node air.js 1 2 3 4 5 4 3 2 1
 // 5
 
-// $> python exo.py bonjour monsieur bonjour
+// $> node air.js bonjour monsieur bonjour
 // monsieur
 
 
@@ -13,5 +13,17 @@
 
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
+// Vérifier si la liste passée en argument est vide ou non une liste.
+// Si la liste est invalide, afficher "Erreur : argument manquant ou invalide" et quitter le programme.
+// Initialiser un tableau vide appelé "occurrences".
+// Parcourir chaque élément dans la liste.
+// Si l'élément est déjà présent dans le tableau "occurrences", augmenter sa valeur de 1.
+// Sinon, ajouter l'élément au tableau "occurrences" avec une valeur de 1.
+// Parcourir chaque élément dans le tableau "occurrences".
+// Si la valeur de l'élément n'est pas pair (c'est-à-dire différente de zéro modulo 2), retourner cet élément.
+// Si aucune valeur non paire n'a été trouvée, retourner null.
+// Récupérer les arguments passés en ligne de commande.
+// Appeler la fonction "trouverValeurNonPaire" en utilisant les arguments récupérés.
+// Si une valeur non paire est renvoyée, l'afficher.
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
