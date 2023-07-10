41aa734013d0f572114f2add85c73233b2940a3c
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-10 23:15:35 +0200

    [Enhancemenet] exo air04 one at a time

--- Air/air04.js
+++ Air/air04.js
@@ -2,14 +2,47 @@
 
 
 // Exemples d’utilisation :
-// $> python exo.py “Hello milady,   bien ou quoi ??”
+// $> node air.js “Hello milady,   bien ou quoi ??”
 // Helo milady, bien ou quoi ?
-
 
 // Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
+// Initialise le résultat avec le premier caractère de la chaîne.
+// Vérifie si le caractère courant est différent du caractère précédent.
+// Ajoute le caractère courant au résultat.
+// Récupère l'argument passé en ligne de commande.
+// Appeler la fonction "supprimerCaracteresAdjacents" en utilisant l'argument.
+// Afficher le résultat.
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function supprimerCaracteresAdjacents(chaine) {
+  if (!chaine || typeof chaine !== "string") {
+    console.error("Erreur : argument manquant ou invalide");
+    return;
+  }
+  
+  // Initialise le résultat avec le premier caractère de la chaîne.
+  let resultat = chaine[0]; 
+  
+  for (let i = 1; i < chaine.length; i++) {
+    // Vérifie si le caractère courant est différent du caractère précédent.
+    if (chaine[i] !== chaine[i - 1]) { 
+       // Ajoute le caractère courant au résultat.
+      resultat += chaine[i];
+    }
+  }  
+  return resultat;
+}
+
+// Récupère l'argument passé en ligne de commande.
+const argument = process.argv[2]; 
+
+// Appeler la fonction "supprimerCaracteresAdjacents" en utilisant l'argument.
+const resultat = supprimerCaracteresAdjacents(argument);
+// Afficher le résultat.
+if (resultat) {
+  console.log(resultat);
+}
