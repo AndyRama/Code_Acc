90ebb31858d9954c91d607f32d13cd80fcd6f688
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-11 22:56:41 +0200

    [Enhancemenet] exo air08 code valide

--- Air/air08.js
+++ Air/air08.js
@@ -26,4 +26,25 @@ function sortedFusion(list1, list2) {
   let index1 = 0;
   let index2 = 0;
 
+  while (index1 < list1.length && index2 < list2.length) {
+    if (list1[index1] < list2[index2]) {
+      mergedList.push(list1[index1]);
+      index1++;
+    } else {
+      mergedList.push(list2[index2]);
+      index2++;
+    }
+  }
+
+  while (index1 < list1.length) {
+    mergedList.push(list1[index1]);
+    index1++;
+  }
+
+  while (index2 < list2.length) {
+    mergedList.push(list2[index2]);
+    index2++;
+  }
+
+  return mergedList;
 }
