3ba82e844d73353acade96c81bb3cc60dd58025d
Andy Ramaroson <andyramaroson@gmail.com>
2023-07-17 23:35:42 +0200

    [enhancement]exo6 parti 1

--- Air/air06.js
+++ Air/air06.js
@@ -29,3 +29,28 @@
 // Si le résultat est défini, afficher le résultat sous forme d'une chaîne de caractères, en séparant chaque élément avec une virgule
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function ma_fonction(array_of_strings, string) {
+  // Vérifier si le tableau array_de_strings est vide, non un tableau ou si la chaîne string est vide ou non une chaîne de caractères.
+  if (!array_of_strings || !Array.isArray(array_de_strings) || array_de_strings.length < 2) {
+    // Si l'un des arguments est invalide, afficher "Erreur : arguments manquants ou invalides".
+    console.error("Erreur : arguments manquants ou invalides");
+    return;
+  }
+
+// Initialiser un tableau vide appelé nouvel_array_de_strings.
+const new_array_of_String = []
+// Parcourir chaque élément dans le tableau array_de_strings.
+for(const element of array_of_strings) {
+  // Si l'élément contient la chaîne string, 
+  if(element.includes(string)){
+    //L'ajouter au tableau nouvel_array_de_strings.
+    new_array_of_String.push(element)
+  }
+}
+// Retourner le tableau nouvel_array_de_strings.
+// Récupérer les arguments passés en ligne de commande.
+// Appeler la fonction ma_fonction en utilisant les arguments récupérés, à l'exception du dernier argument.
+// Stocker le résultat renvoyé par la fonction ma_fonction dans une variable appelée resultat.
+// Si le résultat est défini, afficher le résultat sous forme d'une chaîne de caractères, en séparant chaque élément avec une virgule
+}
