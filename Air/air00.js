992dd923eccede52802772fcc78082c8f64a137e
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-12 22:03:45 +0200

    [fix] reset exercice

--- Air/air00.js
+++ Air/air00.js
@@ -20,20 +20,3 @@
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-function ma_fonction(string_a_couper, string_separateur) {
-  if (!string_a_couper || !string_separateur) {
-    console.error("Erreur : arguments manquants");
-    return;
-  }
-  
-  const tableau = string_a_couper.split(string_separateur);
-  
-  for (let i = 0; i < tableau.length; i++) {
-    console.log(tableau[i]);
-  }
-  
-  return tableau;
-}
-
-ma_fonction("Bonjour les gars", " ");
