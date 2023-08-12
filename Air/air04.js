1eed24c510dfa70b4b8c839d59a5b2fb60670e33
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-12 22:06:13 +0200

    [fix] reset exercice

--- Air/air04.js
+++ Air/air04.js
@@ -18,31 +18,3 @@
 // Afficher le résultat.
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function supprimerCaracteresAdjacents(chaine) {
-  if (!chaine || typeof chaine !== "string") {
-    console.error("Erreur : argument manquant ou invalide");
-    return;
-  }
-  
-  // Initialise le résultat avec le premier caractère de la chaîne.
-  let resultat = chaine[0]; 
-  
-  for (let i = 1; i < chaine.length; i++) {
-    // Vérifie si le caractère courant est différent du caractère précédent.
-    if (chaine[i] !== chaine[i - 1]) { 
-       // Ajoute le caractère courant au résultat.
-      resultat += chaine[i];
-    }
-  }  
-  return resultat;
-}
-
-// Récupère l'argument passé en ligne de commande.
-const argument = process.argv[2]; 
-
-// Appeler la fonction "supprimerCaracteresAdjacents" en utilisant l'argument.
-const resultat = supprimerCaracteresAdjacents(argument);
-// Afficher le résultat.
-if (resultat) {
-  console.log(resultat);
-}
