b5605ed9a740a86e5f91d309d05269ccc62ef976
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-12 22:04:29 +0200

    [fix] reset exercice

--- Air/air01.js
+++ Air/air01.js
@@ -19,20 +19,3 @@
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-function ma_fonction(string_a_couper, string_separateur) {
-  if (!string_a_couper || !string_separateur) {
-    console.error("Erreur : arguments manquants");
-    return;
-  }
-  
-  const tableau = string_a_couper.split(string_separateur);
-  
-  for (let i = 0; i < tableau.length; i++) {
-    console.log(tableau[i]);
-  }
-  
-  return tableau;
-}
-
-ma_fonction("Crevette magique dans la mer des étoiles", "la");
