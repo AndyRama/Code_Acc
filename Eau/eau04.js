2c17f3f0e7904d6456fe159b84ff49721419828e
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-13 14:54:49 +0200

    [fix] reset exercice eau 04

--- Eau/eau04.js
+++ Eau/eau04.js
@@ -9,44 +9,4 @@
 
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
-// Récupére l'argument du fichier 
-// Determiner le "nextPrimeNumber" avec number+ 1 
-// Sinon on affiche -1 si le paramètre est négatif ou mauvais.
-// Afficher le prochain nextPrime
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Récupére l'argument du fichier 
-const number = process.argv[2]
-
-// Determiner le "nextPrimeNumber" avec number+ 1 
-function primeNumber(number) {
-  if (number < 2) {
-    return false;
-  }
-  
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i === 0) {
-      return false;
-    }
-  }  
-  return true;
-}
-
-// Afficher -1 si le paramètre est négatif ou mauvais.
-function nextPrimeNumber(number) {
-  if (number < 0 || isNaN(number)) {
-    return -1;
-  }
-  
-  let nextNumberPrime  = number + 1;
-  
-  while (!primeNumber(nextNumberPrime )) {
-    nextNumberPrime++;
-  }
-  
-  return nextNumberPrime ;
-}
-
-// Afficher le prochain nextPrime
-console.log(nextNumberPrime());
