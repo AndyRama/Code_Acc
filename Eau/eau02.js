e52e120274feccae51d57d8abc318bc0954e6410
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-13 14:52:54 +0200

    [fix] reset exercice eau 02

--- Eau/eau02.js
+++ Eau/eau02.js
@@ -19,23 +19,5 @@
 
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
-// Récupérer les arguments passés en ligne de commande
-// Inverser l'ordre des éléments dans le tableau
-// Afficher les arguments et joindre en une seule chaîne de caractères séparée par des sauts de ligne
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Récupérer les arguments passés en ligne de commande
-const args = process.argv.slice(2);
-
-// version final (refacto)
-let i = 0;
-let result = '';
-
-if (i < args.length) {
-  // Inverser l'ordre des éléments et Afficher les arguments et joindre en une seule chaîne 
-  result = result + args.reverse().join('\n');
-  i++;
-}
-
-console.log(result);
