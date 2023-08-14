76d27f85a97204d7a23f5c32da35df0ce0c5385a
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-14 18:50:15 +0200

    [fix] reset exercice eau 05

--- Eau/eau06.js
+++ Eau/eau06.js
@@ -11,43 +11,4 @@
 
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
-// Déterminer l'argument
-// Si l'argument est présent 
-  // Prendre la chaine de caractère et mettre la lettre en capitalize
-    // Si index est impaire
-    // Sinon return lowercase
-// Sinon afficher error
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-function capitalizeEveryOtherLetter(string) {
-  let result = "";
-  let shouldCapitalize = true;
-
-  for (let i = 0; i < string.length; i++) {
-    const char = string[i];
-    if (/[a-zA-Z]/.test(char)) {
-      if (shouldCapitalize) {
-        result += char.toUpperCase();
-      } else {
-        result += char.toLowerCase();
-      }
-      shouldCapitalize = !shouldCapitalize;
-    } else {
-      result += char;
-    }
-  }
-
-  return result;
-}
-
-const args = process.argv.slice(2);
-
-if (args.length !== 1) {
-  console.log("error");
-  process.exit(1);
-}
-
-const inputString = args[0];
-const resultString = capitalizeEveryOtherLetter(inputString);
-console.log(resultString);
