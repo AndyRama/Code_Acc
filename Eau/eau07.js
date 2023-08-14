124aa6eb7cfb3699216419267152a25750ac76fb
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-14 18:52:20 +0200

    [fix] reset exercice eau 07

--- Eau/eau07.js
+++ Eau/eau07.js
@@ -12,45 +12,4 @@
 
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
-// Vérification du nombre d'arguments
-// Récupération de la chaîne de caractères depuis le premier argument
-// Vérification si la chaîne est vide
-// Créer une fonction pour mettre en majuscule la première lettre d'un mot
-// Séparation de la chaîne de caractères en mots
-// Transformation de chaque mot en mettant en majuscule la première lettre
-// Reconstruction de la chaîne de caractères avec les mots transformés
-// Affichage du résultat
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Vérification du nombre d'arguments
-if (process.argv.length !== 3) {
-  console.error("Error");
-  process.exit(1);
-}
-
-// Récupération de la chaîne de caractères depuis le premier argument
-const inputString = process.argv[2];
-
-// Vérification si la chaîne est vide
-if (inputString.length === 0) {
-  console.error("Error");
-  process.exit(1);
-}
-
-// Fonction pour mettre en majuscule la première lettre d'un mot
-function capitalizeFirstLetter(word) {
-  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-}
-
-// Séparation de la chaîne de caractères en mots
-const words = inputString.split(/\s+/);
-
-// Transformation de chaque mot en mettant en majuscule la première lettre
-const capitalizedWords = words.map(capitalizeFirstLetter);
-
-// Reconstruction de la chaîne de caractères avec les mots transformés
-const outputString = capitalizedWords.join(" ");
-
-// Affichage du résultat
-console.log(outputString);
