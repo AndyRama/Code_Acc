aa9bed781d451336536e6b7800ad2be9c4454019
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-15 21:48:46 +0200

    [fix] reset exercice ea 10

--- Eau/eau10.js
+++ Eau/eau10.js
@@ -15,42 +15,5 @@
 
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
-// Afficher error et quitter le programme en cas de problèmes d’arguments.
-// Récupérer  tout les arguments exepter le dernier 
-// Récupérer l'argument rechercher   ["charlie"]
-// Parcourir le tableau pour trouver l'index de l'élement recherché
-// Afficher l'index
-// Afficher -1 si l’élément n’est pas trouvé
-// Afficher 0 si pas assez d'arguments
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Afficher error et quitter le programme en cas de problèmes d’arguments.
-if(process.argv.length < 3 ){
-  console.log('Erreur : Pas d\'arguments.');
-  process.exit(1)
-}
-
-//Récupérer  tout les arguments exepter le dernier
-const elements = process.argv.slice(2,-1) //["Ceci" ,"est" ,"le","monde" ,"qui" ,"contient" ,"Charlie" ,"un" ,"homme" ,"sympa"]
-
-//Récupérer l'argument rechercher   ["charlie"]
-const recherche = process.argv[process.argv.length - 1 ]
-
-//Parcourir le tableau pour trouver l'index de l'élement recherché
-let index = -1 
-for (let i =0; i < elements.length; i++) {
-  if(elements[i] === recherche) {
-    index = i; 
-    break;
-  }
-}
-
-//Afficher l'index
-//Afficher -1 si l’élément n’est pas trouvé
-//Afficher 0 aucun argument
-
-console.log(`${index}`);
-
-// console.log(`Tableau : [${elements }]`);
-// console.log(`Recherche : [${recherche}]`);
