dfcb5aa8770b63e52a37a32b3ab99e15013f1fd0
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-28 22:29:35 +0200

    [enhancement] function

--- Eau/eau01.js
+++ Eau/eau01.js
@@ -14,3 +14,14 @@
 // On appelle la fonction qui va vérifier si les chiffres sont différents les uns des autres.
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// On crée une fonction qui va vérifier si les chiffres sont différents les uns des autres.
+function allCombi(combi) {
+  const digits = combi.split('')
+  const uniqueDigits = [ ...new Set(digits)] 
+  return digits.length == uniqueDigits.length
+}
+  // On crée une boucle for qui va de 0 à 99 pour le deuxième premier chiffres.   [i] = 00
+  // On crée une boucle for qui va de 0 à 99 pour le deuxième suivant chiffres.   [j] = 00
+  // On crée une variable qui va contenir les chiffres de la combinaison.
+  // On appelle la fonction qui va vérifier si les chiffres sont différents les uns des autres.
