03aa670746386c02918a22270b0856d9763f61a8
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-28 22:13:33 +0200

    [enhancement] pseudo code for combinaison of 3 numbers

--- Eau/eau00.js
+++ Eau/eau00.js
@@ -16,4 +16,11 @@
 
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
+// On crée une fonction qui va vérifier si les chiffres sont différents les uns des autres.
+// On crée une boucle for qui va de 0 à 9 pour le premier chiffre.    [i] = 0
+// On crée une boucle for qui va de 0 à 9 pour le deuxième chiffre.   [j] = [i] + 1
+// On crée une boucle for qui va de 0 à 9 pour le troisième chiffre.  [k] = [j] + 1
+// On crée une variable qui va contenir les chiffres de la combinaison.
+// On appelle la fonction qui va vérifier si les chiffres sont différents les uns des autres.
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
