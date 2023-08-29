04458872a6c2069b9ca7828e4ba5e4c4891a086e
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-29 22:53:23 +0200

    [enhancement] curl for numbers

--- Eau/eau01.js
+++ Eau/eau01.js
@@ -21,12 +21,20 @@ function allCombi(combi) {
   const uniqueDigits = [ ...new Set(digits)] 
   return digits.length == uniqueDigits.length
 }
-  // On crée une boucle for qui va de 0 à 99 pour le deuxième premier chiffres.   [i] = 00
-  for( let i = 0; i <= 99; i++) {
-    // On crée une boucle for qui va de 0 à 99 pour le deuxième suivant chiffres.   [j] = 00
-    for(let j = 0 && i >= 99; j <= 99; j++) {
+
+let result = [];
+
+for (let i = 0; i <= 99; i++) {
+  for (let j = 0  && i >= 99; j <= 99; j++) {
+
+    let combi = `${i}${j}`;
+
+      if (allCombi(combi)) { 
+        if (i < 10 && j < 10) {
+          combi = `0${i}0${j}`;
+
+        result.push(combi);
+    }
   }
 }
-  // On crée une variable qui va contenir les chiffres de la combinaison.
-  let combi = `${i}${j}`
-  // On appelle la fonction qui va vérifier si les chiffres sont différents les uns des autres.
+console.log(result.join(', '));
