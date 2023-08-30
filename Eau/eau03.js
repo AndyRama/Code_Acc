08284894e0bc4230282982a0565327a78e003990
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-30 14:34:50 +0200

    [enhancement] suite Fibonacci

--- Eau/eau03.js
+++ Eau/eau03.js
@@ -16,3 +16,27 @@
 // Boucle iteration en fonction de(i , n )
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Récupérer l'argument
+const n = process.argv[2]
+
+// console.log(n);
+
+// Crée une fonction fib(n) n etant l'argument passer ligne de cmd et newFib
+let newFib = fib(n)
+
+function fib(n) {
+  // Initialisation tableau
+  let result = [0, 1]
+
+  // Vérifier mauvais cas et nombre négatif
+  if (n <= 0  || n === undefined) { return '-1 : erreur argument'}
+    // Boucle itération en fonction de(i , n )
+    for (let i = 2; i <= n; i++) {
+      let nextValue = result[i - 1] + result[i - 2]
+      result.push(nextValue)
+    }
+    return result
+  }
+
+console.log(newFib.join(' '))
