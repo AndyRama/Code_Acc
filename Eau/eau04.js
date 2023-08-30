df2856eed1e74b783183aaa2492dfddd79fa12de
Andy Ramaroson <andyramaroson@gmail.com>
2023-08-30 23:45:27 +0200

    [enhancement] boucle

--- Eau/eau04.js
+++ Eau/eau04.js
@@ -10,8 +10,27 @@
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
 // Récupére l'argument du fichier 
-// Determiner le "nextPrimeNumber" avec number+ 1 
+// Determiner le "nextPrimeNumber" avec number + 1 
 // Sinon on affiche -1 si le paramètre est négatif ou mauvais.
 // Afficher le prochain nextPrime
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Récupére l'argument du fichier 
+const number = process.argv[2]
+
+// Determiner le "nextPrimeNumber" avec number + 1 
+function PrimeNumber(number){
+  if( i < 2 ) {
+    return false
+  }
+
+  for(let i = 2; i <= Math.sqrt(number); i++) {
+    if(number % i === 0) {
+      return false
+    }
+  }
+  return true
+}
+// Sinon on affiche -1 si le paramètre est négatif ou mauvais.
+// Afficher le prochain nextPrime
