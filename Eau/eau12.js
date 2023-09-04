78a30f074bca92c6c773ae50c3708a3d568b61eb
Andy Ramaroson <andyramaroson@gmail.com>
2023-09-04 22:14:07 +0200

    [enhancement] function work correctly

--- Eau/eau12.js
+++ Eau/eau12.js
@@ -6,7 +6,7 @@
 // 	return (new_array)
 // }
 
-// Exemples d’utilisation :
+// Exemples d’utilisation
 // $> node eau12.js 6 5 4 3 2 1
 // 1 2 3 4 5 6
 
@@ -38,13 +38,35 @@ if(process.argv.length < 3 ) {
   process.exit(1)
 }
 
-// Récupérer les chiffres à partir des argurments
+// Récupérer les chiffres à partir des arguments dans un nouveau tableau
 const arg = process.argv.slice(2).map(number)
 
+console.log(arg);
+
 // Vérifier sinous avons que des nombre(ISNAN)
+if(arg.some(isNaN)) {
+  console.log("Erreur : veuillez spécifier une liste de nombre");
+  process.exit(1)
+}
 
 // Fonction de tri à bulle
+function ordered(arg) {
+  const n = arg.length;
+  for(let i = 0; i < n - 1; i++) {
+    for(let j = 0; j < n - i - 1; j++) {
+      if(arg[j] > arg[j + 1]) {
+        //Echanger les éléments
+        const max = arg[j]
+        arg[j] = arg[ j + 1]
+        arg[j + 1] = max
+      }
+    }
+  }
+  return arg
+}
 
 // Trie des nombres
+const result = ordered(arg).join(' ');
 
 // Affichage des nombres triés
+console.log(result);
