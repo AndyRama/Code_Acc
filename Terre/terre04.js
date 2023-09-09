12c8875720fe9a978cf1757cb5d0647c5f0759f2
Andy Ramaroson <andyramaroson@gmail.com>
2023-09-09 23:03:33 +0200

    [enhancement] function number pair or impair

--- Terre/terre04.js
+++ Terre/terre04.js
@@ -1,3 +1,5 @@
+// nombre entier pair ou impaire ou pas un nombre entier
+
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
 //argurment est un nombre entier pair ou impaire ou pas un nombre entier
@@ -8,3 +10,27 @@
 //si le argument pas de arguments args[0]
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//argurment est un nombre entier pair ou impaire ou pas un nombre entier
+const args = process.argv.slice(2)
+
+let i = 0
+while(i < args.length ){
+  //si le nombre est divisible par 2
+  if(args[i] % 2 === 0) {
+    console.log("nombre est pair");
+  }
+
+  //sinon si le nombre n'est pas divisible par 2
+  else if(args[i] % 2 === 1) {
+    console.log("nombre est impaire");
+  }
+
+  //sinon si le nombre est negatif
+  else if(args[i] < 0) {
+    console.log("ce nombre est negatif");
+  }
+
+  //sinon l'argument n'est pas un nombre
+  //si le argument pas de arguments args[0]
+}
