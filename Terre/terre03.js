50ed0fd4bfc6782064a312ac1a2da6235606565f
Andy Ramaroson <andyramaroson@gmail.com>
2023-09-11 20:53:59 +0200

    [enhancement] pseudo code

--- Terre/terre03.js
+++ Terre/terre03.js
@@ -1,5 +1,8 @@
 
 // Créez un programme qui affiche l’alphabet à partir de la lettre donnée en argument en lettres minuscules suivi d’un retour à la ligne.
+// Exemples d’utilisation :
+// $> python exo.py n
+// nopqrstuvwxyz
 
 const args = process.argv.slice(2);
 
