f27a42755c871b52019bcec3e814d27cb38f7048
Andy Ramaroson <andyramaroson@gmail.com>
2023-09-11 20:49:55 +0200

    [fix] error function result

--- Terre/terre01.js
+++ Terre/terre01.js
@@ -1,6 +1,14 @@
+// Créez un programme qui affiche son nom de fichier.
+// Exemples d’utilisation :
+
+// $> node exo.js
+// exo.js
+
+// $> node crevette.js
+// crevette.js
+
 ///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
 
-// Créez un programme qui affiche son nom de fichier.
 
 
 
@@ -8,5 +16,10 @@
 
 // Créez un programme qui affiche son nom de fichier.
 
-const name = "Terre01.js"
-console.log(name);
+const args = process.argv[1];
+
+// /home/andyrama/code/AndyRama/Code_Acc/JavaScript/Terre/Terre01.js
+
+return console.log(args.split("/").pop());
+
+// Terre01.js 
--- Terre/terre05.js
+++ Terre/terre05.js
@@ -2,7 +2,7 @@
 
 // Division deux de nombre entier afiichage de la division et du reste
 // DIVISION ERROR [REFACTO CONDITION ELSE IF]
-//RESULTAT ET RESTE DE LA DIVISION SANS DECIMAL
+// RESULTAT ET RESTE DE LA DIVISION SANS DECIMAL
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
