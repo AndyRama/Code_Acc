d393c65bd147fa7abe18f9496e721c8f42e92277
Andy Ramaroson <andyramaroson@gmail.com>
2023-09-11 20:57:31 +0200

    [enhancement] pseudo code

---
+++ Terre/terre07.js
@@ -0,0 +1,25 @@
+// Créez un programme qui affiche le nombre de caractères d’une chaîne de caractères passée en argument.
+
+// Exemples d’utilisation :
+// $> node Terre07.js “Hello world!”
+// 12
+
+
+// $> node Terre07.js
+// erreur.
+
+
+// $> node Terre07.js “Bonjour” “Aurevoir”
+// erreur.
+
+// $> node Terre07.js 10
+// erreur.
+
+///////////////////////////////////////////////// Pseudo code ////////////////////////////////////////////////////////////////
+
+//Recupérer les arguments
+//Prendre le premier argument est afficher la longeur de la chaine pour le nb caractere
+//Prendre le second argument est afficher une erreur
+//Return result
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
